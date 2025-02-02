a967e5c477bcdecba46bfb566f33ae5560ce2ddb
pushpak-2005 <pushpakdighore@gmail.com>
2025-02-02 22:25:32 +0530

    Add files via upload

---
+++ sentiment analysis.py
@@ -0,0 +1,25 @@
+from textblob import TextBlob
+
+print("welcome to sentiment analysing tool")
+
+print("Enter the 'stop' to stop the program")
+
+user_input=(input("enter your text: "))
+while user_input.lower() !="stop":
+  sentence=TextBlob(user_input) 
+  if sentence.sentiment.polarity >=0.1 :
+       print("the sentence is positive. ")
+  elif sentence.sentiment.polarity >=-0.1 or sentence.sentiment.polarity <=0.1 :
+       print("the sentence is neutral.") 
+  else : print (" the sentence is negative.")
+     
+  user_input=input("enter another statement: ")
+
+print("thanks for using the tool.")
+
+
+
+
+    
+
+
